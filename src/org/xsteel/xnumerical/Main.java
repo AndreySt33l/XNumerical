679e69d8b0b5c90a069f03c327cbab5923e54266
AndreySt33l <stalnoyandrey.private@gmail.com>
2019-10-22 09:51:52 +0600

    Basics added

---
+++ src/org/xsteel/xnumerical/Main.java
@@ -0,0 +1,17 @@
+package org.xsteel.xnumerical;
+
+public class Main {
+    /**
+     * todo: class Function based on Map
+     *       decide whether HashMap or LinkedHashMap
+     * todo: class Partition
+     * todo: class Difference Schemes \
+     * todo: class Error
+     * todo: Exact and Approximate Solutions
+     * ------ Representation Part --------
+     * todo: class Table
+     * todo: class Plot
+     * */
+    public static void main(String[] args) {
+    }
+}
